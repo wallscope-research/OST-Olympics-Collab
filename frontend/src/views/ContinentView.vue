207f3d3c80e243037f01b46035976217d2b6eaca
johnnystrachan <js113@hw.ac.uk>
2020-08-27 14:50:45 +0100

    Continent line chat working

--- frontend/src/views/ContinentView.vue
+++ frontend/src/views/ContinentView.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
 .home
   .title-page
-    icon(:icon="['fas', 'globe-europe']")
+    icon(:icon='["fas", "globe-europe"]')
     h3 Europe
   .charts
     .one
       InfoBox
     .two
-
+      MultipleLines(:propOptions='getOptions', :title='title')
     .three
-      
+
     .four
       News
 </template>
@@ -18,30 +18,63 @@
 import { Component, Vue } from 'vue-property-decorator';
 import InfoBox from '@/components/InfoBox.vue';
 import News from '@/components/News.vue';
-@Component({ components: { InfoBox, News } })
-export default class ContinentView extends Vue {}
+import MultipleLines from '@/components/MultipleLines.vue';
+@Component({ components: { InfoBox, News, MultipleLines } })
+export default class ContinentView extends Vue {
+  medals = {
+    2000: { numOfAtheltes: 1358, numOfMedals: 416 },
+    2002: { numOfAtheltes: 1437, numOfMedals: 527 },
+    2004: { numOfAtheltes: 1673, numOfMedals: 558 },
+    2006: { numOfAtheltes: 1622, numOfMedals: 582 },
+    2008: { numOfAtheltes: 1469, numOfMedals: 504 },
+    1998: { numOfAtheltes: 1232, numOfMedals: 463 },
+    2010: { numOfAtheltes: 1398, numOfMedals: 448 },
+    2012: { numOfAtheltes: 1442, numOfMedals: 315 },
+    2014: { numOfAtheltes: 1565, numOfMedals: 387 },
+    2016: { numOfAtheltes: 1577, numOfMedals: 428 },
+    2018: { numOfAtheltes: 1622, numOfMedals: 489 },
+  };
+
+  title = 'Number of athletes vs number of medals won';
+  get getOptions() {
+    const data = Object.keys(this.medals);
+    const series = [];
+    series.push({
+      name: 'Number of athletes',
+      data: Object.values(this.medals).map((x) => x.numOfAtheltes),
+      type: 'line',
+    });
+    series.push({
+      name: 'Number of meals awarded',
+      data: Object.values(this.medals).map((x) => x.numOfMedals),
+      type: 'line',
+    });
+    return { data, series };
+  }
+}
 </script>
 
 
 <style lang="scss" scoped>
-
-@media only screen and (max-width: 768px ) {
+@media only screen and (max-width: 768px) {
   .home {
     .charts {
       grid-template-columns: 1fr;
     }
   }
 }
-@media only screen and (min-width:769px) and (max-width:1300px){
+@media only screen and (min-width: 769px) and (max-width: 1300px) {
   .home {
-    .charts { grid-template-columns: 1fr; }
+    .charts {
+      grid-template-columns: 1fr;
+    }
   }
 }
 @media only screen and (min-width: 1301px) {
   .home {
     .charts {
       grid-template-columns: 0.4fr 0.6fr 1fr;
-      >div {
+      > div {
         &.four {
           grid-row: 1/3;
           grid-column: 3/4;
